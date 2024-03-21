d9c9d7a559c66e8cbfa422c6bf90448743304ca2
Frank Torres <frank.torres@retabet.es>
2024-03-21 12:47:29 +0100

    Minor changes: Refactor InfoController - variable names and the order of the public methods

--- InformationMonitor/InfoController.cs
+++ InformationMonitor/InfoController.cs
@@ -3,14 +3,18 @@ namespace InformationMonitor;
 public class InfoController
 {
     private static DateTime NowMinus(TimeSpan timeSpan) => DateTime.Now.Subtract(timeSpan);
-    private readonly TimeSpan TenMinutes = new(0, 10, 0);
-    private Timer? continuousMonitoring;
-    private readonly int oneSecondInMs = 1000;
+	private readonly TimeSpan TenMinutes = new(0, 10, 0);
+	private readonly TimeSpan OneSecond = new(0, 0, 1);
+	private Timer? continuousMonitoring;
 
-    public InfoController(IInfoReader Reader, IInfoDispatcher Dispatcher)
+	public IInfoReader Reader { get; set; }
+
+	public IInfoDispatcher Dispatcher { get; set; }
+
+	public InfoController(IInfoReader reader, IInfoDispatcher dispatcher)
     {
-        this.Reader = Reader;
-        this.Dispatcher = Dispatcher;
+        this.Reader = reader;
+        this.Dispatcher = dispatcher;
     }
 
     public void MonitorLastTenMinutes()
@@ -25,7 +29,9 @@ public class InfoController
     /// <exception cref="InvalidOperationException">If previous monitoring is runnning</exception>
     public void MonitorEverySecond()
     {
-        if (this.continuousMonitoring is not null)
+		int oneSecondInMs = 1000;
+
+		if (this.continuousMonitoring is not null)
             throw new InvalidOperationException("Continuous monitoring is running. Stop it before running another.");
 
         this.continuousMonitoring = new Timer(this.ContinuousMonitoringCallback, null, 0, oneSecondInMs);
@@ -35,7 +41,7 @@ public class InfoController
     {
         try
         {
-            List<Info> infoEntries = Reader.GetInformationSince(NowMinus(TenMinutes));
+            List<Info> infoEntries = Reader.GetInformationSince(NowMinus(OneSecond));
             Dispatcher.Dispatch(infoEntries);
         }
         catch
@@ -51,7 +57,4 @@ public class InfoController
     }
 
     public bool IsContinuousMonitoringRunning => this.continuousMonitoring != null;
-
-    public IInfoReader Reader { get; set; }
-    public IInfoDispatcher Dispatcher { get; set; }
 }
