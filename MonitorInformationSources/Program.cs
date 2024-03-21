feb436612a96e91e517f7fdfacd5c837011f7542
Frank Torres <frank.torres@retabet.es>
2024-03-21 12:39:12 +0100

    Minor changes to program: Split code into separated Methods, Change local buffer acting as IInfoDispatcher

--- MonitorInformationSources/Program.cs
+++ MonitorInformationSources/Program.cs
@@ -8,66 +8,85 @@ namespace MonitorInformationSources;
 [SupportedOSPlatform("windows")]
 class Program
 {
-    static void Main(string[] args)
-    {
-        var sourceSelection = "all";
+	static void Main(string[] args)
+	{
+		string selectionFromArgs = ParseArguments(args);
 
-        if (args.Any()) sourceSelection = args[0].ToLower();
+		Action runSample = SelectSample(selectionFromArgs);
 
-        switch(sourceSelection)
-        {
-            case "event": PrintLastLogs(new EventLogReader(), new ConsoleDispatcher(ParsingActions.ForConsole)); break;
-            case "process": PrintLastLogs(new ProcessesReader(), new ConsoleDispatcher(ParsingActions.ForConsole)); break;
-            default: PrintLastLogsFromSeveralSources(new List<IInformationSourceProxy>
-            {
-                new EventLogReader(),
-                new ProcessesReader()
-            },
-            new ConsoleDispatcher(ParsingActions.ForConsole)
-            ); break;
-        }        
-    }
+		runSample();
+	}
 
-    private static void PrintLastLogsFromSeveralSources(List<IInformationSourceProxy> readingSources, IInfoDispatcher dispatcher)
-    {
-        var bufferedDispatcher = new NoDispatchButBufferDispatcher();
-        readingSources.ForEach(source => {
-            var monitorController = new InfoController(new InfoReader(source), bufferedDispatcher);
-            monitorController.MonitorLastTenMinutes();
-        });
+	private static string ParseArguments(string[] args)
+	{
+		var sourceSelection = "all";
 
-        PrintHeaderIntoDispatcher(dispatcher, $"Last 10 minutes of information from: {string.Join(", ", readingSources.Select(s => s.GetType().Name))}");
-        dispatcher.Dispatch(bufferedDispatcher.RecoverEntriesOrderedByTime());
-    }
+		if (args.Any())
+			sourceSelection = args[0].ToLower();
+		return sourceSelection;
+	}
 
-    private static void PrintLastLogs(IInformationSourceProxy readingSource, IInfoDispatcher dispatcher)
-    {
-        PrintHeaderIntoDispatcher(dispatcher, $"Last 10 minutes of information from: {readingSource.GetType().Name}");
+	private static Action SelectSample(string sourceSelection)
+	{
+		Action sample;
 
-        var monitorController = new InfoController(new InfoReader(readingSource), dispatcher);
-        monitorController.MonitorLastTenMinutes();
-    }
+		switch (sourceSelection)
+		{
+			case "event": sample = () => PrintLastLogs(new EventLogReader(), new ConsoleDispatcher(ParsingActions.ForConsole)); break;
+			case "process": sample = () => PrintLastLogs(new ProcessesReader(), new ConsoleDispatcher(ParsingActions.ForConsole)); break;
+			default:
+				sample = () => PrintLastLogsFromSeveralSources(new List<IInformationSourceProxy>
+				{
+					new EventLogReader(),
+					new ProcessesReader()
+				},
+				new ConsoleDispatcher(ParsingActions.ForConsole)
+				); break;
+		}
+		return sample;
+	}
 
-    private static void PrintHeaderIntoDispatcher(IInfoDispatcher dispatcher, string message)
-    {
-        dispatcher.Dispatch(new List<Info> {
-            new Info {
-                 Moment = DateTime.Now,
-                 Level = Definitions.ReportedLevel.Normal,
-                 Information = message
-            }
-        });
-    }
+	private static void PrintLastLogsFromSeveralSources(List<IInformationSourceProxy> readingSources, IInfoDispatcher dispatcher)
+	{
+		var bufferedDispatcher = new NoDispatchButBufferAllEntries();
+		readingSources.ForEach(source =>
+		{
+			var monitorController = new InfoController(new InfoReader(source), bufferedDispatcher);
+			monitorController.MonitorLastTenMinutes();
+		});
+
+		PrintHeaderIntoDispatcher(dispatcher, $"Last 10 minutes of information from: {string.Join(", ", readingSources.Select(s => s.GetType().Name))}");
+		dispatcher.Dispatch(bufferedDispatcher.RecoverEntriesOrderedByTime());
+	}
+
+	private static void PrintLastLogs(IInformationSourceProxy readingSource, IInfoDispatcher dispatcher)
+	{
+		PrintHeaderIntoDispatcher(dispatcher, $"Last 10 minutes of information from: {readingSource.GetType().Name}");
+
+		var monitorController = new InfoController(new InfoReader(readingSource), dispatcher);
+		monitorController.MonitorLastTenMinutes();
+	}
+
+	private static void PrintHeaderIntoDispatcher(IInfoDispatcher dispatcher, string message)
+	{
+		dispatcher.Dispatch(new List<Info> {
+			new Info {
+				 Moment = DateTime.Now,
+				 Level = Definitions.ReportedLevel.Normal,
+				 Information = message
+			}
+		});
+	}
 }
 
-class NoDispatchButBufferDispatcher : IInfoDispatcher
+class NoDispatchButBufferAllEntries : IInfoDispatcher
 {
-    private List<Info> bufferedEntries = new List<Info>();
+	private List<Info> bufferedEntries = new List<Info>();
 
-    public void Dispatch(List<Info> infoEntries)
-    {
-        this.bufferedEntries.AddRange(infoEntries);
-    }
+	public void Dispatch(List<Info> infoEntries)
+	{
+		this.bufferedEntries.AddRange(infoEntries);
+	}
 
-    public List<Info> RecoverEntriesOrderedByTime() =>  bufferedEntries.OrderBy(e => e.Moment).ToList();
+	public List<Info> RecoverEntriesOrderedByTime() => bufferedEntries.OrderBy(e => e.Moment).ToList();
 }
